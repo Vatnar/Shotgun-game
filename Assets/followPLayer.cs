30a88bb429cffd1f8c273e8c15705044f346d790
Peter Vatnar Andersson <peter@70x7.net>
2024-01-13 18:54:44 +0100

    jiopiojjiojio

--- Assets/followPLayer.cs
+++ Assets/followPLayer.cs
@@ -3,16 +3,14 @@ using UnityEngine;
 public class followPLayer : MonoBehaviour {
     [SerializeField] private Transform camTransform;
     private Vector3 position;
-    private void Awake() {
-        position = camTransform.position;
-    }
+    
     void Start() {
-        transform.position = new Vector3(position.x, position.y, 0);
+        transform.position = new Vector3(camTransform.position.x, camTransform.position.y, 0);
     }
 
     // Update is called once per frame
     void Update() {
-        transform.position = new Vector3(position.x, position.y, 0);
+        transform.position = new Vector3(camTransform.position.x, camTransform.position.y, 0);
 
     }
 }
