23b58a18ad04bd07ac1fe2d80d12f735d09163c0
Vatnar <peter@70x7.net>
2024-04-16 15:59:40 +0200

    Moving platforms pog

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,19 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    [SerializeField] private
-    // Start is called before the first frame update
+    [SerializeField] private Vector2 offset = new Vector2(0,0);
+    [SerializeField] private float period = 1f;
+    Vector2 startPosition;
+    float t = 0;
+    bool up = true;
     void Start()
     {
+        startPosition = transform.position;
 
     }
 
-    // Update is called once per frame
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Vector3 scale = new Vector3(transform.localScale.x*3, transform.localScale.y / 2, 0);
+        Gizmos.DrawCube(transform.position+(Vector3)offset, scale);
+    }
+
     void Update()
     {
+
+        if (t > period || !up)
+        {
+            t -= Time.deltaTime;
+            up = false;
+        }
+        else {t += Time.deltaTime;}
+
+        if (t < 0) { up = true;}
+
+
+        transform.position = Vector2.Lerp(startPosition, startPosition+offset, t/period);
 
     }
 }
